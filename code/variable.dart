984ffc7e41c864febf6f877650c091a9e7961fd2
kenvp299 <josuevp2699@gmail.com>
2024-01-17 16:11:26 -0500

    welcome

---
+++ code/variable.dart
@@ -0,0 +1,21 @@
+void main(){
+
+//final significa que el valor de la variable no va a cambiar
+String bebida ='';
+final String plato = 'Ceviche';
+String? adicional;
+final List<int> precio = [1,2,3,4,5];
+
+
+adicional = 'porcion de pan';
+//otra forma de imprimir usando 3 comillas dobles
+for (var i = 0; i < precio.length; i++) {
+  print(precio[i]);
+}
+print(
+plato+" "+
+bebida+" "+
+adicional
+);
+}
+
