7ddd859d12eab078d62374473eb995580aaea064
kevin-lucas07 <dvjoao42@gmail.com>
2024-01-17 17:33:35 -0500

    tipo de dato dynamic

--- material/02_tipos_variables.dart
+++ material/02_tipos_variables.dart
@@ -5,11 +5,19 @@ void main(){
   final List<String> abilities = ['kill mouse']; //Lista String tipado comun
   final sprites = <String>['gato.front.png','gato.back.png']; //Lista String otro tipo de tipado (Ambas listas son validas)
 
+  dynamic errorMessage = 'Hola'; //se recomienda usar dynamic solo en casos muy especificos debido a quita multiples restricciones
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5, 6];
+  errorMessage = {1, 2, 3, 4, 5, 6};
+  errorMessage = () => true;
+  errorMessage = null;
+
   print("""
   $animal
   $edad
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   """);
 }
