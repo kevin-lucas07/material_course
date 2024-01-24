8fca7b1a22bab8e286dfa9801b853a8fc424746c
kevin-lucas07 <dvjoao42@gmail.com>
2024-01-24 14:58:35 -0500

    Ejemplo de clase y constructor mas eficiente

--- material/06_classes.dart
+++ material/06_classes.dart
@@ -1,18 +1,23 @@
-void main(){
+void main() {
 
-  final Wolverine = new Hero('Logan','Regeneration');
+  final Wolverine = new Hero(name: 'Logan');
 
   print(Wolverine);
   print(Wolverine.name);
   print(Wolverine.power);
-
 }
 
-class Hero{
-  
+class Hero {
   String name;
   String power;
 
-  Hero(this.name, this.power);
-  
+  Hero({
+    required this.name,
+    this.power = 'Sin Poder'
+  });
+
+  //Forma de convertir la instancia para imprimir los valores
+  String toString(){
+    return '$name - $power';
+  }
 }
