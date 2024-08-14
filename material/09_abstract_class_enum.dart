9cdbe9192b804d75cf4d0da8b253581d7ae9c87b
kevin-lucas07 <dvjoao42@gmail.com>
2024-08-14 16:10:10 -0500

    uso de inmplements

--- material/09_abstract_class_enum.dart
+++ material/09_abstract_class_enum.dart
@@ -1,23 +1,54 @@
-void main(){
+void main() {
+  final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
 
+  print('wind: ${chargePhone(windPlant)}');
+  print('nuclear: ${chargePhone(nuclearPlant)}');
 }
 
-enum PlantType {
-  nuclear,
-  wind,
-  water
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('No enough energy');
+  }
+
+  return plant.energyLeft - 10;
 }
+
+enum PlantType { nuclear, wind, water }
 
 abstract class EnergyPlant {
-  
   double energyLeft;
-  PlantType type;
+  final PlantType type;
 
-  EnergyPlant({
-    required this.energyLeft,
-    required this.type
-  });
+  EnergyPlant({required this.energyLeft, required this.type});
 
   void consumeEnergy(double amount);
+}
 
+//extends
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
+
+//Implements se utiliza para usar componentes en especificos de una clase abstracta
+class NuclearPlant implements EnergyPlant {
+  
+  @override
+  double energyLeft;
+
+  @override
+  final PlantType type = PlantType.nuclear;
+
+  NuclearPlant({ required this.energyLeft});
+
+  @override
+  void consumeEnergy(double amount){
+    energyLeft -= (amount * 0.5);
+  }
 }
