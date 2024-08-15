828595fc0c962b40f52e159455d9c5196836a939
kevin-lucas07 <dvjoao42@gmail.com>
2024-08-15 17:07:56 -0500

    uso de async await

---
+++ material/12_async_await.dart
@@ -0,0 +1,19 @@
+void main() async {
+  print('Inicio del programa');
+
+  try {
+    final value = await httpGet('https://ejemplo.com');
+    print(value);
+  } catch (err) {
+    print('Tenemos un error: $err');
+  }
+
+  print('Fin del programa');
+}
+
+//Future para el uso de promesas o asincronia de funciones.
+Future<String> httpGet(String url) {
+  return Future.delayed(const Duration(seconds: 1), () {
+    throw 'Error en la peticion http';
+  });
+}
