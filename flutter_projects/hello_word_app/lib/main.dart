ec7868d609ec3824650ab49ee597e3c8be58fbf1
kevin-lucas07 <dvjoao42@gmail.com>
2024-10-09 15:52:52 -0500

    creacion de screen en diferentes archivos

--- flutter_projects/hello_word_app/lib/main.dart
+++ flutter_projects/hello_word_app/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hello_word_app/presentation/screens/counter/counter_screen.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Hola Mundo')),
-    )
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen()
     );
   }
 }
