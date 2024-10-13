2e410349dec99a8b14195a99e5fa6f5348655657
kevin-lucas07 <dvjoao42@gmail.com>
2024-10-13 14:01:05 -0500

    scaffold, column, text_style, floating_action_button

--- flutter_projects/hello_word_app/lib/presentation/screens/counter/counter_screen.dart
+++ flutter_projects/hello_word_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
-
 class CounterScreen extends StatelessWidget {
   const CounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(child: Text('Counter Screen')),
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),),
+            Text('Clicks', style: TextStyle(fontSize: 25),)],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
     );
   }
 }
