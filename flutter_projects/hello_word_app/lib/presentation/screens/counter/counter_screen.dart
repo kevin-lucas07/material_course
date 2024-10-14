87f8258a2509281e4f311aaf408262976e511ad6
kevin-lucas07 <dvjoao42@gmail.com>
2024-10-14 16:02:55 -0500

    condicion con operador ternario

--- flutter_projects/hello_word_app/lib/presentation/screens/counter/counter_screen.dart
+++ flutter_projects/hello_word_app/lib/presentation/screens/counter/counter_screen.dart
@@ -28,9 +28,9 @@ class _CounterScreenState extends State<CounterScreen> {
               style:
                   const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            const Text(
-              'Clicks',
-              style: TextStyle(fontSize: 25),
+            Text(
+              'Click${ clickCounter > 1 ? 's' : ''}',
+              style: const TextStyle(fontSize: 25),
             )
           ],
         ),
